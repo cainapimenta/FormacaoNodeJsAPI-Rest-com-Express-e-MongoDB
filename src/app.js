4eb6d58a4ee9712a18f882ee765a44575ca9243c
Cainã Pimenta <caina.pimenta@alfaerp.com.br>
2024-12-08 03:17:53 -0300

    07 - Buscando e atualizando livros

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ const livros = [
     }
 ];
 
+function buscarLivro(id){
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js Express");
 });
@@ -22,9 +28,29 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+    const index = buscarLivro(req.params.id);
+
+    if(index === -1)
+        res.status(404).send();
+    else
+        res.status(200).json(livros[index]);
+});
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).json(req.body);
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscarLivro(req.params.id);
+
+    if(index === -1)
+        res.status(404).send();
+    else {
+        livros[index].titulo = req.body.titulo;
+        res.status(204).send();
+    }
 });
 
 export default app;
